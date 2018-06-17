3c5cfadf7e257621d376ba432f3dde57e9bf97dc
MeghaPrashanth <megharaj109@gmail.com>
2018-06-17 16:14:45 -0700

    Business Mortgage and Personal Mortgage

---
+++ src/Exercise3/BusinessMortgage.java
@@ -0,0 +1,18 @@
+package Exercise3;
+
+public class BusinessMortgage extends  Mortgage {
+
+    BusinessMortgage(String Mortgage_Number,String Customer_Name,double Mortage_Amount,double Interest_Rate,int term)
+    {
+        super(Mortgage_Number,Customer_Name,Mortage_Amount,(Interest_Rate+1),term);
+    }
+
+
+
+    @Override
+    public  String getMortgageInfo()
+    {
+        return String.format("Business Mortgage: %s",
+                super.toString());
+    }
+}
---
+++ src/Exercise3/PersonalMortgage.java
@@ -0,0 +1,16 @@
+package Exercise3;
+
+public class PersonalMortgage extends  Mortgage {
+
+   public PersonalMortgage(String Mortgage_Number,String Customer_Name,double Mortage_Amount,double Interest_Rate,int term)
+    {
+        super(Mortgage_Number,Customer_Name,Mortage_Amount,(Interest_Rate+2),term);
+    }
+
+    @Override
+    public  String getMortgageInfo()
+    {
+        return String.format("Business Mortgage: %s",
+                super.toString());
+    }
+}
